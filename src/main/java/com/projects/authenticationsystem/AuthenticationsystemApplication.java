3347bc2a59feac7dc128e693ee9422c366ea4e72
Luiz Fernando <luizfernandosant26@gmail.com>
2025-06-17 12:19:32 -0300

    Project created

---
+++ src/main/java/com/projects/authenticationsystem/AuthenticationsystemApplication.java
@@ -0,0 +1,11 @@
+package com.projects.authenticationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationsystemApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationsystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/projects/authenticationsystem/AuthenticationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.authenticationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
