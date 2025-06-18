329cd790307f8262c5a969cfae079e005c9e27dc
Luiz Fernando <luizfernandosant26@gmail.com>
2025-06-17 21:10:18 -0300

    feat/ Delete method

--- src/main/java/com/projects/authenticationsystem/resources/UserResource.java
+++ src/main/java/com/projects/authenticationsystem/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,5 +50,11 @@ public class UserResource {
 	public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User obj) {
 		obj = service.updateUser(id, obj);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+		service.deleteUser(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/projects/authenticationsystem/services/UserService.java
+++ src/main/java/com/projects/authenticationsystem/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projects.authenticationsystem.entities.User;
 import com.projects.authenticationsystem.repositories.UserRepository;
+import com.projects.authenticationsystem.services.exceptions.DatabaseException;
 import com.projects.authenticationsystem.services.exceptions.ResourceNotFoundException;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -34,16 +37,28 @@ public class UserService {
 	public User updateUser(Long id, User obj) {
 		try {
 			User entity = repository.getReferenceById(id);
-			updateData(entity, obj);
+			updateUser(entity, obj);
 			return repository.save(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException(id);
 		}
 	}
 
-	private void updateData(User entity, User obj) {
+	public void updateUser(User entity, User obj) {
 		entity.setUsername(obj.getUsername());
 		entity.setEmail(obj.getEmail());
 		entity.setPassword(obj.getPassword());
+	}
+
+	public void deleteUser(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 }
