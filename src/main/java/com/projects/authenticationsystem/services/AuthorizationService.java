335773d4bf47688f6024586c3739f4abc89efabe
Luiz Fernando <luizfernandosant26@gmail.com>
2025-06-17 22:17:18 -0300

    feat/ Authorization service

--- src/main/java/com/projects/authenticationsystem/services/AuthorizationService.java
+++ src/main/java/com/projects/authenticationsystem/services/AuthorizationService.java
@@ -1,8 +1,21 @@
 package com.projects.authenticationsystem.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.projects.authenticationsystem.repositories.UserRepository;
+
 @Service
-public class AuthorizationService {
-    
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username);
+    }
 }
